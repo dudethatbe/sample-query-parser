6a1412dfd477e1a4b79fe21093e4eee76c01ddcd
saroj990 <sarojsasmal990@gmail.com>
2017-12-14 21:56:12 +0530

    update logic

--- app.js
+++ app.js
@@ -1,47 +1,37 @@
-var query = {
-  DBA:"False",
-  Distributor:"Distributors",
-  limit:"5000",
-  offset:"0",
-  Owner:"match=/chris/i",
-  Priority:"ne=Fast Track",
-  Tags:"match=/new/i"
-};
+generateDynamicQueryCondition: (queryParams) => {
+  //filter out offset and limit
+  let paramKeys = Object.keys(queryParams).filter((key) => !['offset', 'limit'].includes(key));
+  
+  //simply return empty string when no filter key is passed in
+  if(!paramKeys.length) return ``;  
 
-function parseAndGenerateSqlQuery(query){
-  let keys = Object.keys(query)
-             .filter( (key) =>  !['limit', 'offset'].includes(key));
-  
-  let queryObj = '';
-  
-  var count = 0;
-  keys.forEach((key) => {
+  let queryObj = ` WHERE `, count = 0;
+
+  paramKeys.forEach((paramKey) => {
+    if(count) queryObj +=  ` AND `;
     
-    if(count != 0) queryObj += ' AND ';
+    let paramValue = queryParams[paramKey];
+    //Add the key to query object
+    queryObj += `name='${paramKey}' AND `;
 
-    let value = query[key];
-    let v = '';
-    queryObj += `( name = '${key}' AND `
-
-
-     
-     if(value.includes('match=')){
-        v =  value.split('/')[1]
-        queryObj += ` value LIKE '${v}' `;
-     }
-    else if( value.includes('ne=')){
-        v =  value.split('=')[1];
-        queryObj += ` value <> '${v}' `
+    //pattern match 
+    if(paramValue.includes('match')){
+      let value = paramValue.split('/')[1];
+      queryObj += (value.toLowerCase() === 'nobody') ? 
+                              (`(value = '' OR value IS NULL)`) : (`value LIKE '%${value}%'`)
     }
+    //not equal to matcher 
+    else if(paramValue.includes('ne')){
+      let value = paramValue.split('=')[1];
+      queryObj += `value <> '${value}'`;
+    }
+    //equality match
     else{
-      queryObj += ` value = '${value}' ` 
+      queryObj += `value = '${paramValue}'`;
     }
-    queryObj += ` ) `
+    // queryObj += `)`
     count++;
   });
-  
+
   return queryObj;
 }
-
-var data = parseAndGenerateSqlQuery(query);
-console.log(data);
